fddd15fe4a0c9be5b82d6ef11b1de31c6b552fae
markfastner <markfastner@hotmail.com>
2023-02-26 09:40:38 -0800

    marks commit(db and navbar)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
-import LandingPage from "./views";
+import NavBar from "./navbar/NavBar.jsx";
+import Dashboard from "./dashboard/Dashboard";
+import "./firebase.js";
+import { GoogleAuthProvider } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 function App() {
+  
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-    <LandingPage />
+    <div className="App">
+      <NavBar />
+      <Dashboard />
+    </div>
   );
 }
 
