d8ed54b58aec9152cdc6816397c3fa9be31a0878
Porter Clevidence <porterclev@gmail.com>
2023-02-26 14:27:45 -0800

    discover hardcode

--- src/views/Discover/map.js
+++ src/views/Discover/map.js
@@ -1,18 +1,11 @@
 import { GoogleMap, LoadScript, MarkerF } from "@react-google-maps/api";
-import { useState } from "react";
-import { getFirestore } from "firebase/firestore";
-import { doc, setDoc, getDoc } from "firebase/firestore";
+import { useState, useEffect } from "react";
+import { Firestore, getFirestore } from "firebase/firestore";
+import { doc, setDoc, getDoc, collection } from "firebase/firestore";
 import { getApp } from "firebase/app";
-
-// const MarkerFrame = ({ lat, lng }) => {
-//   return <MarkerF key="Marker" position={{ lat, lng }} />;
-// };
-
-// const db = getFirestore(getApp());
+import { getDb } from "../../firebase.js";
 
 const MapContainer = () => {
-  // const [info, setInfo] = useState([]);
-
   const mapStyles = {
     height: "600px",
     width: "800px",
@@ -23,38 +16,34 @@ const MapContainer = () => {
     lng: -118.11376327323174,
   };
 
-  // const trainerMarker1 = db.setDoc(
-  //   doc(getFirestore(), "Markers", "Trainer 1"),
-  //   {
-  //     lat: "33.78359095823877",
-  //     lng: "-118.13001185467596",
-  //   }
-  // );
-  // const trainerMaker2 = db.setDoc(doc(getFirestore(), "Markers", "Trainer 2"), {
-  //   lat: "33.79016988590503",
-  //   lng: "-118.10729130628533",
-  // });
+  const findAll = async () => {
+    const doc_refs = await getDoc(collection(getFirestore(), "Markers"));
+    const res = [];
 
-  // const docSnap = db.getDoc(trainerMarker1);
-  // // Start the fetch operation as soon as
-  // // the page loads
-  // window.addEventListener("load", () => {
-  //   Fetchdata();
-  // });
+    doc_refs.forEach((marker) => {
+      res.push({
+        id: marker.id,
+        ...marker.data(),
+      });
+    });
 
-  // Fetch the required data using the get() method
-  // const Fetchdata = () => {
-  //   db.collection("Markers")
-  //     .get()
-  //     .then((querySnapshot) => {
-  //       // Loop through the data and store
-  //       // it in array to display
-  //       querySnapshot.forEach((element) => {
-  //         var data = element.data();
-  //         setInfo((arr) => [...arr, data]);
-  //       });
-  //     });
-  // };
+    return res;
+  };
+
+  const [loading, setLoading] = useState(false);
+  const [markers, setMarkers] = useState([]);
+  const fetchData = async () => {
+    setLoading(true);
+
+    const res = await findAll();
+
+    setMarkers([...res]);
+    setLoading(false);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  console.log(markers);
   return (
     <LoadScript googleMapsApiKey="AIzaSyC7wxgZWkIeg5_SopuRAryCEvpL5n4lm0E">
       <GoogleMap
@@ -63,9 +52,13 @@ const MapContainer = () => {
         center={defaultCenter}
         position={{ lat: 47.444, lng: -122.176 }}
       >
-        {/* {info.map((data) => (
-          <MarkerFrame lat={data.lat} lng={data.lng} />
-        ))} */}
+        {/* {markers.length > 0 &&
+          markers.map((marker) => (
+            <MarkerF
+              key={marker.name}
+              position={{ lat: marker.lat, lng: marker.lng }}
+            />
+          ))} */}
         <MarkerF
           key="Location"
           position={{ lat: 33.78129604720617, lng: -118.11376327323174 }}
