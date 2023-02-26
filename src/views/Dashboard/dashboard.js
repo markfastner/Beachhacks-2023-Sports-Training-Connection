defbb99b2af28ab2caf57f538b481865f3b1940f
Porter Clevidence <porterclev@gmail.com>
2023-02-25 19:18:52 -0800

    icons

--- src/views/Dashboard/dashboard.js
+++ src/views/Dashboard/dashboard.js
@@ -1,11 +1,75 @@
 import "./dashboard.css";
 import React, { Component } from "react";
 import { StyleSheet, View, Text } from "react-native";
+import MaterialCommunityIconsIcon from "react-native-vector-icons/MaterialCommunityIcons";
+import IoniconsIcon from "react-native-vector-icons/Ionicons";
+import MaterialIconsIcon from "react-native-vector-icons/MaterialIcons";
 
 const dashboard = () => {
   const styles = StyleSheet.create({
     container: {
       flex: 1,
+    },
+    icon: {
+      color: "rgba(128,128,128,1)",
+      fontSize: 40,
+      height: 43,
+      width: 40,
+    },
+    icon2: {
+      color: "rgba(128,128,128,1)",
+      fontSize: 40,
+      height: 44,
+      width: 35,
+      marginLeft: 38,
+    },
+    icon3: {
+      color: "rgba(128,128,128,1)",
+      fontSize: 40,
+      height: 44,
+      width: 33,
+      marginLeft: 39,
+    },
+    icon4: {
+      color: "rgba(128,128,128,1)",
+      fontSize: 40,
+      height: 44,
+      width: 33,
+      marginLeft: 38,
+    },
+    iconRow: {
+      height: 44,
+      flexDirection: "row",
+      marginTop: 46,
+      marginLeft: 41,
+      marginRight: 54,
+    },
+    icon5: {
+      color: "rgba(128,128,128,1)",
+      fontSize: 40,
+      height: 43,
+      width: 40,
+    },
+    icon6: {
+      color: "rgba(128,128,128,1)",
+      fontSize: 40,
+      height: 40,
+      width: 40,
+      marginLeft: 38,
+    },
+    icon7: {
+      color: "rgba(128,128,128,1)",
+      fontSize: 40,
+      height: 43,
+      width: 40,
+      marginLeft: 34,
+    },
+    icon5Row: {
+      height: 43,
+      flexDirection: "row",
+      marginTop: 41,
+      marginLeft: 41,
+      marginRight: 118,
     },
     rect: {
       width: 461,
@@ -75,7 +139,40 @@ const dashboard = () => {
       </View>
 
       <View style={styles.rect2Row}>
-        <View style={styles.rect2}></View>
+        <View style={styles.rect2}>
+          <View style={styles.iconRow}>
+            <MaterialCommunityIconsIcon
+              name="swim"
+              style={styles.icon}
+            ></MaterialCommunityIconsIcon>
+            <IoniconsIcon
+              name="ios-fitness"
+              style={styles.icon2}
+            ></IoniconsIcon>
+            <IoniconsIcon
+              name="ios-basketball"
+              style={styles.icon3}
+            ></IoniconsIcon>
+            <IoniconsIcon
+              name="ios-american-football"
+              style={styles.icon4}
+            ></IoniconsIcon>
+          </View>
+          <View style={styles.icon5Row}>
+            <MaterialCommunityIconsIcon
+              name="volleyball"
+              style={styles.icon5}
+            ></MaterialCommunityIconsIcon>
+            <MaterialIconsIcon
+              name="directions-bike"
+              style={styles.icon6}
+            ></MaterialIconsIcon>
+            <MaterialCommunityIconsIcon
+              name="home"
+              style={styles.icon7}
+            ></MaterialCommunityIconsIcon>
+          </View>
+        </View>
         <View style={styles.rect4}></View>
         <View style={styles.rect5}></View>
       </View>
