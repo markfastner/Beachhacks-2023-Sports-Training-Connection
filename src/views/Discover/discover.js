dd8943b01b130f4107e4bdf571089760d6ede84d
Porter Clevidence <porterclev@gmail.com>
2023-02-26 09:40:18 -0800

    finished discover wireframe

--- src/views/Discover/discover.js
+++ src/views/Discover/discover.js
@@ -1,21 +1,88 @@
-import GoogleMapReact from "google-map-react";
 import Map from "./map";
+import "./discover.css";
+import styled from "styled-components";
+import {
+  FaFootballBall,
+  FaBasketballBall,
+  FaSwimmer,
+  FaVolleyballBall,
+  FaRunning,
+} from "react-icons/fa";
+import { IoIosFitness } from "react-icons/io";
+import { TbBike } from "react-icons/tb";
+
 const discover = () => {
-  return <Map />;
-  // const location = {
-  //   address: "1600 Amphitheatre Parkway, Mountain View, california.",
-  //   lat: 37.42216,
-  //   lng: -122.08427,
-  // };
-  // return (
-  //   <div>
-  //     <Map
-  //       location={location}
-  //       zoomLevel={10}
-  //       style={{ height: "100vh", width: "100%" }}
-  //     />
-  //   </div>
-  // );
+  const Container = styled.div`
+    display: flex;
+    background-color: #212121;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+    border-radius: 2px;
+    min-width: 88px;
+    padding-left: 16px;
+    padding-right: 16px;
+    box-shadow: 0px 1px 5px 0.35px #000;
+    height: 36px;
+    margin: 10px;
+  `;
+
+  const Caption = styled.span`
+    font-family: Roboto;
+    color: #fff;
+    font-size: 20px;
+  `;
+
+  return (
+    <section className="discover-container">
+      <div className="map-container">
+        <Map />
+      </div>
+      <div className="discover-categories">
+        <Container>
+          <Caption>
+            <FaFootballBall />
+          </Caption>
+        </Container>
+
+        <Container>
+          <Caption>
+            <FaBasketballBall />
+          </Caption>
+        </Container>
+
+        <Container>
+          <Caption>
+            <FaSwimmer />
+          </Caption>
+        </Container>
+
+        <Container>
+          <Caption>
+            <FaVolleyballBall />
+          </Caption>
+        </Container>
+
+        <Container>
+          <Caption>
+            <FaRunning />
+          </Caption>
+        </Container>
+
+        <Container>
+          <Caption>
+            <IoIosFitness />
+          </Caption>
+        </Container>
+
+        <Container>
+          <Caption>
+            <TbBike />
+          </Caption>
+        </Container>
+      </div>
+    </section>
+  );
 };
 
 export default discover;
--- src/views/Discover/map.js
+++ src/views/Discover/map.js
@@ -1,8 +1,8 @@
 import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 const MapContainer = () => {
   const mapStyles = {
-    height: "100vh",
-    width: "80%",
+    height: "600px",
+    width: "800px",
   };
 
   const defaultCenter = {
@@ -17,8 +17,9 @@ const MapContainer = () => {
         zoom={13}
         center={defaultCenter}
         position={{ lat: 47.444, lng: -122.176 }}
-      />
-      <Marker key="marker_1" position={{ lat: 41.3851, lng: 2.1734 }} />
+      >
+        <Marker key="marker_1" position={{ lat: 41.3851, lng: 2.1734 }} />
+      </GoogleMap>
     </LoadScript>
   );
 };
